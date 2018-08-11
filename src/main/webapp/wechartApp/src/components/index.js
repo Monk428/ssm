525bc753ac9ed6495cd93102f532d243f00e1ac3
wengjf <wengjianfen@126.com>
2018-08-11 16:09:13 +0800

    merge

--- src/main/webapp/wechartApp/src/components/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by 680701 on 2017/8/4.
- */
-import Hello from './Hello.vue'
-export  default{
-  components:Hello
-}
--- src/main/webapp/wechartApp/static/root/root.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 680701 on 2017/8/4.
- */
-window.greeRoot="http://yun.worldours.com";
-const Root = {
-  api:{
-    login:window.greeRoot+'/rest/banner/getBanners',
-  }
-}
